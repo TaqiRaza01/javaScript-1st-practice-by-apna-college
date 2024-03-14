c2f0b12cd667441b94883cbce28a53c05d2f895a
Taqi Raza <taqidaim01@gmail.com>
2024-03-14 13:34:11 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Taqi Raza");
+const profile={
+    fullName: "TaqiRaza",
+    followers: 243,
+    post: 1234,
+    Bio: "My name is Taqi and I am learning JavaScript",
+    isfolow: false
+    
+}
+console.log(profile)   
+console.log(profile.fullName)
+console.log(typeof profile)   
+console.log(typeof profile.Bio)   
